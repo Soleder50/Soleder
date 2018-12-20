aa62e3efd4e6efe6b4e833b90e67777f75444741
Soleder50 <46032931+Soleder50@users.noreply.github.com>
2018-12-20 22:28:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("525075522217312278")
 setInterval(function() {
 channel.send(`B0B yala B0B yala B0B yala B0B yalaB0B yala B0B yalaB0B yala B0B yala`);
-}, 30)
+}, 15)
 })
 
 client.login(process.env.BOT_TOKEN);
