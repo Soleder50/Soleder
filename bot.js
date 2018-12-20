cdc2c92a1f1f2c2df4ed93560ba5e16a4ebb4c9a
Soleder50 <46032931+Soleder50@users.noreply.github.com>
2018-12-20 22:18:10 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("525075522217312278")
+setInterval(function() {
+channel.send(`B0B yala B0B yala B0B yala B0B yalaB0B yala B0B yalaB0B yala B0B yala`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
